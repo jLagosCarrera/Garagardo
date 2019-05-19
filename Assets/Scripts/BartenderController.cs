a130da5ab49016321e83776d87475cf53f5d445e
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-05-19 22:39:57 +0200

    Add previous changes
    They are on readme, I deleted what I've got and started over on another local repo with same name, thats why i couldnt make the commits on this repo

---
+++ Assets/Scripts/BartenderController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class BartenderController : MonoBehaviour
+{
+    private bool hasMoved = false;
+    private readonly float[] positions = { -5.5f, -2.5f, 2.5f, 5.5f };
+    private int currentPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwipeManager.OnSwipeDetected += OnSwipeDetected;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnSwipeDetected(Swipe direction, Vector2 swipeVelocity)
+    {
+        float tileHeight;
+
+        switch (direction)
+        {
+            case Swipe.Up:
+                if (!hasMoved)
+                {
+                    currentPosition = 2;
+                    transform.position = new Vector3(transform.position.x, positions[currentPosition], transform.position.z);
+                    hasMoved = true;
+                    break;
+                }
+
+                break;
+            case Swipe.Down:
+                if (!hasMoved)
+                {
+                    currentPosition = 1;
+                    transform.position = new Vector3(transform.position.x, positions[currentPosition], transform.position.z);
+                    hasMoved = true;
+                    break;
+                }
+
+                break;
+            default:
+                //Do nothing hehe
+                break;
+        }
+    }
+}
