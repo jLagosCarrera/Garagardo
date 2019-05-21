79eb9ba54a367ccd199efb0ec58fffb17df2aa7f
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-05-21 19:01:51 +0200

    Added beer throwing and collision at the end of the table
    At 3 errors, you die

--- Assets/Scripts/BartenderController.cs
+++ Assets/Scripts/BartenderController.cs
@@ -2,6 +2,13 @@
 
 public class BartenderController : MonoBehaviour
 {
+    Vector2 beerPos;
+    public GameObject beer;
+    public GameObject dieMenuUI;
+    public static int lifes = 3;
+    public float throwRate = 0.5f;
+    float nextBeer = 0.0f;
+
     private bool hasMoved = false;
     private readonly float[] positions = { -3.5f, -1.5f, 1.5f, 3.5f };
     private int currentPosition;
@@ -9,13 +16,19 @@ public class BartenderController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        lifes = 3;
+        Time.timeScale = 1f;
         SwipeManager.OnSwipeDetected += OnSwipeDetected;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (lifes == 0)
+        {
+            dieMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+        }
     }
 
     void OnSwipeDetected(Swipe direction, Vector2 swipeVelocity)
@@ -28,7 +41,7 @@ public class BartenderController : MonoBehaviour
                     if (!hasMoved)
                     {
                         currentPosition = 2;
-                        transform.position = new Vector3(transform.position.x, positions[currentPosition], transform.position.z);
+                        transform.position = new Vector2(transform.position.x, positions[currentPosition]);
                         hasMoved = true;
                         break;
                     }
@@ -36,7 +49,7 @@ public class BartenderController : MonoBehaviour
                     if (currentPosition < positions.Length - 1)
                     {
                         currentPosition++;
-                        transform.position = new Vector3(transform.position.x, positions[currentPosition], transform.position.z);
+                        transform.position = new Vector2(transform.position.x, positions[currentPosition]);
                     }
 
                     break;
@@ -56,10 +69,24 @@ public class BartenderController : MonoBehaviour
                     }
 
                     break;
-                default:
-                    //Do nothing hehe
+                case Swipe.Left:
+                    if (hasMoved)
+                    {
+                        if (Time.time > nextBeer)
+                        {
+                            nextBeer = Time.time + throwRate;
+                            throwBeer();
+                        }
+                    }
                     break;
             }
         }
+    }
+
+    void throwBeer()
+    {
+        beerPos = transform.position;
+        beerPos += new Vector2(-0.3f, 0f);
+        Instantiate(beer, beerPos, Quaternion.identity);
     }
 }
---
+++ Assets/Scripts/BeerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeerController : MonoBehaviour
+{
+    public GameObject beerCollider;
+    public float velX = -5f;
+    float velY = 0f;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(velX, velY);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "BeerCollider")
+        {
+            BartenderController.lifes--;
+            Destroy(gameObject);
+        }
+    }
+}
